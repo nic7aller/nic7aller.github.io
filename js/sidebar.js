0552de0b8c0c7ac44f3c11b6ed1b1fbfcc3ee8ff
Nic 7aller <n.7aller@gmail.com>
2017-05-28 18:10:00 -0400

    Minor style changes and testing sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+/*! Source: W3Schools */
+
+function openNav() {
+    document.getElementById("theSidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("theSidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
