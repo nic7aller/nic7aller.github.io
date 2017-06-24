aff3c14d53cd3e6e65189a19086798985d7f1cc2
Nic 7aller <n.7aller@gmail.com>
2017-06-24 14:10:06 -0400

    Fixed GPA and more sidebar testing

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,25 @@
-/*! Source: W3Schools */
+/*! Source: W3Schools; modified */
 
 function openNav() {
-    document.getElementById("theSidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    $(".nav-close-link").css("padding-left", "0px");
+    $(".link").fadeIn();
+    $(".navbar-brand").css("width", "auto");
+    $("#theSidenav").width("250px");
+    $("#main").css("margin-left", "250px");
+    $("#closeArrow").removeClass("fa-angle-right");
+    $("#closeArrow").addClass("fa-angle-left");
 }
 
 function closeNav() {
-    document.getElementById("theSidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    $(".nav-close-link").css("padding-left", "-16px");
+    $(".link").fadeOut();
+    $(".navbar-brand").css("width", "0");
+    $("#theSidenav").width("50px");
+    $("#main").css("margin-left", "50px");
+    $("#closeArrow").removeClass("fa-angle-left");
+    $("#closeArrow").addClass("fa-angle-right");
 }
+
+$(".closebtn").click(function() {
+    $("#closeArrow").hasClass("fa-angle-left") ? (closeNav()) : (openNav());
+});
