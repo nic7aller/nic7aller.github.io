5313b47706fe2a1b32eb5faac868d05adf11f7ac
Nic Siebenaller <n.7aller@gmail.com>
2018-04-12 15:36:13 -0400

    Overlay displays correctly

--- js/overlay.js
+++ js/overlay.js
@@ -4,7 +4,7 @@ $(document).on("click",".image-button-right", function (event) {
     var id = div_area.attr('id');
     closeOverlay();
     displayCorrectOverlay(id);
-    $("#overlay").css({'width':'40%','right':'40%', 'visibility':'visible'});
+    $("#overlay").addClass("out");
  });
 
  $(document).on("click",".image-button-left", function (event) {
@@ -13,14 +13,12 @@ $(document).on("click",".image-button-right", function (event) {
     var id = div_area.attr('id');
     closeOverlay();
     displayCorrectOverlay(id);
-    $("#overlay").css({'width':'40%','left':'40%','visibility':'visible'});
+    $("#overlay").addClass("out");
  });
 
  function closeOverlay() {
-    if ($("#overlay").hasClass("right")) {
-        $("#overlay").css({'width':'0','left':'100%', 'visibility':'hidden'});
-    } else {
-        $("#overlay").css({'width':'0','right':'100%', 'visibility':'hidden'});
+    if ($("#overlay").hasClass("out")) {
+        $("#overlay").removeClass("out");
     }
  }
 
@@ -28,23 +26,23 @@ $(document).on("click",".image-button-right", function (event) {
     if (id == "westminster") {
         displayWestminster();
     } else if (id == "central") {
-         
+        displayCentral();
     } else if (id == "changing") {
-         
-    } else if (id == "victoria") {
-         
-    } else if (id == "meeting") {
-         
+        displayRobing();
+    } else if (id == "prince") {
+         displayPrince();
+    } else if (id == "peers") {
+         displayPeers();
     } else if (id == "gallery") {
-         
+         displayGallery();
     } else if (id == "lords") {
-         
+         displayLords();
     } else if (id == "pm") {
-         
+        displayPM();
     } else if (id == "commons") {
-         
-    } else if (id == "yay") {
-         
+        displayCommons();
+    } else if (id == "aye") {
+        displayAye();
     }
  }
 
@@ -52,7 +50,128 @@ $(document).on("click",".image-button-right", function (event) {
     $("#overlay").replaceWith('<div id="overlay" class="left">' +
     '<div class="overlay-header">' +
         '<a onclick="closeOverlay()"><span class="fa fa-angle-left fa-2x"/><span class="overlay-title slab-font">Westminster Hall</span></a>' +
-    '</div><div class="overlay-body">' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayCentral() {
+    $("#overlay").replaceWith('<div id="overlay" class="left">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-left fa-2x"/><span class="overlay-title slab-font">Central Lobby</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayPM() {
+    $("#overlay").replaceWith('<div id="overlay" class="left">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-left fa-2x"/><span class="overlay-title slab-font">Members\' Lobby</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayAye() {
+    $("#overlay").replaceWith('<div id="overlay" class="left">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-left fa-2x"/><span class="overlay-title slab-font">Aye Lobby</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayCommons() {
+    $("#overlay").replaceWith('<div id="overlay" class="left">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-left fa-2x"/><span class="overlay-title slab-font">Commons Chamber</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayRobing() {
+    $("#overlay").replaceWith('<div id="overlay" class="right">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-right fa-2x"/><span class="overlay-title slab-font">Robing Room</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayGallery() {
+    $("#overlay").replaceWith('<div id="overlay" class="right">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-right fa-2x"/><span class="overlay-title slab-font">Royal Gallery</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayPeers() {
+    $("#overlay").replaceWith('<div id="overlay" class="right">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-right fa-2x"/><span class="overlay-title slab-font">Peers\' Lobby</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayPrince() {
+    $("#overlay").replaceWith('<div id="overlay" class="right">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-right fa-2x"/><span class="overlay-title slab-font">Prince\'s Chamber</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+    '</div></div>');
+ }
+
+ function displayLords() {
+    $("#overlay").replaceWith('<div id="overlay" class="right">' +
+    '<div class="overlay-header">' +
+        '<a onclick="closeOverlay()"><span class="fa fa-angle-right fa-2x"/><span class="overlay-title slab-font">Lords Chamber</span></a>' +
+    '</div><div id="custom-scroll" class="overlay-body">' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
+        '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
         '<div class="overlay-pod"><p>Random text in Latin</p></div>' +
     '</div></div>');
  }
