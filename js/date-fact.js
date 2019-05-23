7c31a67a74ab8a7d813045d83dc81cb8aff16aa7
Nic Siebenaller <n.7aller@gmail.com>
2019-05-23 16:31:12 -0400

    Resolves CORS issue with Numbers API

--- js/date-fact.js
+++ js/date-fact.js
@@ -1,4 +1,4 @@
 var d = new Date();
-$.get("http://numbersapi.com/" + (d.getMonth() + 1)+ "/" + d.getDate() + "/date?default=Something+cool+probably+happened+today.", function(data) {
+$.get("https://cors-anywhere.herokuapp.com/http://numbersapi.com/" + (d.getMonth() + 1)+ "/" + d.getDate() + "/date?default=Something+cool+probably+happened+today.", function(data) {
     $("#datefact").text(data.toString());
 });
