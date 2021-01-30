03e3d75f2926f85886edc43ad318c063b0a56fcc
Nic <n.7aller@gmail.com>
2021-01-30 12:10:34 -0800

    Initial CRA commit

--- js/advice.js
+++
@@ -1,4 +0,0 @@
-$.getJSON("https://api.adviceslip.com/advice", function(data) {
-    var advice = data.slip.advice
-    $("#advice").text(advice);
-});
--- js/date-fact.js
+++
@@ -1,9 +0,0 @@
-var d = new Date();
-jQuery.ajaxPrefilter(function(options) {
-    if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-});
-$.get("http://numbersapi.com/" + (d.getMonth() + 1)+ "/" + d.getDate() + "/date?default=Something+cool+probably+happened+today.", function(data) {
-    $("#datefact").text(data.toString());
-});
